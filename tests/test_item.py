0619b0afca885d77b836071e35619ddffe8524b9
Анастасия <bobina_asya@mail.ru>
2024-07-16 13:50:44 +0300

    functions and tests for them have been written

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import *
+
+def test_calculate_total_price():
+    item = Item("Наушники", 5000, 5)
+    assert item.calculate_total_price() == 25000
+
+def test_apply_discount():
+    item = Item("Наушники", 5000, 5)
+    item.apply_discount()
+    assert item.price == item.price
